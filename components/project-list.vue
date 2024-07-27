993e689fed89e9a6f2f1dca0167438480583f65f
Ooi Kim Siah <kimsiahooi@gmail.com>
2024-07-27 21:24:00 +0800

    complete 20

---
+++ components/project-list.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>projects</div>
+
+  <section v-if="pending">Loading...</section>
+  <section v-else-if="error">Something went wrong... Try again!</section>
+  <section v-else>Here we display the repos!</section>
+</template>
+
+<script setup lang="ts">
+const { error, pending, data } = useFetch('https://api.github.com/users/piotr-jura-udemy/repos')
+</script>
+
+<style scoped></style>
