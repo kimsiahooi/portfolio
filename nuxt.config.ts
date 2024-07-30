b5c36501fae2d8bcb15c4dd933ce810f6861a6a3
Ooi Kim Siah <kimsiah@fomatictech.com>
2024-07-30 08:36:51 +0800

    complete 29

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,16 +2,19 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/tailwindcss',
-    '@nuxtjs/color-mode',
-    "@nuxt/content",
-    "@nuxt/image"
-  ],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode', '@nuxt/content', '@nuxt/image'],
   typescript: {
     typeCheck: true,
   },
   colorMode: {
     classSuffix: '',
+  },
+  content: {
+    highlight: {
+      theme: {
+        default: 'github-light',
+        dark: 'github-dark',
+      },
+    },
   },
 })
