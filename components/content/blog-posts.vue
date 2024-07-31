4a76f18cb283bbd26832716f5749a4537f7998c2
Ooi Kim Siah <kimsiah@fomatictech.com>
2024-07-31 08:47:45 +0800

    complete 33

--- components/content/blog-posts.vue
+++ components/content/blog-posts.vue
@@ -8,7 +8,13 @@
     <ul>
       <li v-for="post in posts" :key="post._path">
         <NuxtLink :to="post._path" class="column hover:bg-gray-100 dark:hover:bg-gray-800">
-          <div class="text-gray-500">2023</div>
+          <div
+            :class="{
+              'text-white dark:text-gray-900': !post.displayYear,
+              'text-gray-400 dark:text-gray-500': post.displayYear,
+            }">
+            {{ post.year }}
+          </div>
           <div>{{ post.title }}</div>
         </NuxtLink>
       </li>
@@ -17,12 +23,32 @@
 </template>
 
 <script setup lang="ts">
-const { data: posts } = useAsyncData('blog-list', () =>
+const { data } = useAsyncData('blog-list', () =>
   queryContent('/blog')
     .where({ _path: { $ne: '/blog' } })
-    .only(['_path', 'title'])
+    .only(['_path', 'title', 'publishedAt'])
+    .sort({ publishedAt: -1 })
     .find()
 )
+
+const posts = computed(() => {
+  if (!data.value) {
+    return []
+  }
+
+  const result = []
+  let lastYear = null
+
+  for (const post of data.value) {
+    const year = new Date(post.publishedAt).getFullYear()
+    const displayYear = year !== lastYear
+    const tempResult = { ...post, displayYear, year }
+    result.push(tempResult)
+    lastYear = year
+  }
+
+  return result
+})
 </script>
 
 <style scoped>
